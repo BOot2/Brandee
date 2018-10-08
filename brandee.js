63006538a030363453c2ff5cfa91d2219667692c
BOot2 <43100466+BOot2@users.noreply.github.com>
2018-10-08 18:01:23 +0300

    Update brandee.js

--- brandee.js
+++ brandee.js
@@ -700,6 +700,32 @@ if (!rank) return message.reply('انت لا تمتلك الرتبه المخص�
 
 });
 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+  
+ 
+
+if (command == "embed") {
+let rank = message.guild.member(message.author).roles.find('name', 'Say');
+if (!rank) return message.reply('انت لا تمتلك الرتبه المخصصه لهذا الامر')
+    let say = new Discord.RichEmbed()
+    .setDescription(args.join("  "))
+    .setColor(0x23b2d6)
+    message.channel.sendEmbed(say);
+    message.delete();
+  }
+
+
+});
+
+
+
 
 client.login(process.env.BOT_TOKEN);
 
