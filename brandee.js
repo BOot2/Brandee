285dc8d0c92718c7dff8fd6e83cb09871c990d89
BOot2 <43100466+BOot2@users.noreply.github.com>
2018-10-08 17:53:30 +0300

    Update brandee.js

--- brandee.js
+++ brandee.js
@@ -688,11 +688,17 @@ client.on('message', message => {
 
   let args = message.content.split(" ").slice(1);
 
-  if (command == "!say") {
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
+
+
+if (command == "say") {
+let rank = message.guild.member(message.author).roles.find('name', 'Say');
+if (!rank) return message.reply('انت لا تمتلك الرتبه المخصصه لهذا الامر')
+  message.channel.send(args.join("  "))
+    message.delete();
   }
- });
+
+
+});
 
 
 client.login(process.env.BOT_TOKEN);
