d79a4270321228893db33a8362f16785fd5f7de7
BOot2 <43100466+BOot2@users.noreply.github.com>
2018-10-08 13:16:04 +0300

    Update brandee.js

--- brandee.js
+++ brandee.js
@@ -123,18 +123,20 @@ client.on('message', message => {
 
 
 
-  client.on('message', message => {
-
+    client.on('message', message => {
+    
     if (message.content === "!hchannel") {
-                        if(!message.channel.guild) return message.reply(' This command only for servers');
-
-if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' ليس لديك صلاحيات');
-           message.channel.overwritePermissions(message.guild.id, {
-         READ_MESSAGES: false
-           }).then(() => {
-               message.reply("تم اخفاء الشات✅ ");
-	 
-           });
+    if(!message.channel.guild) return message.reply(' This command only for servers');
+    
+    if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' ليس لديك صلاحيات');
+    message.channel.overwritePermissions(message.guild.id, {
+    READ_MESSAGES: false
+    }).then(() => {
+    message.reply("✅ ");
+    
+    });
+    }
+});// remove this 
 
 
 
@@ -150,7 +152,9 @@ if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' لي
            }).then(() => {
                message.reply("تم اظهار الشات✅ ");
 	   
-           });
+  });
+    }
+});// remove this 
 
 
 
