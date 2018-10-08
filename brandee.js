a77c890f9ad8f5ed2f2183cf5d7cfac8df7a5916
BOot2 <43100466+BOot2@users.noreply.github.com>
2018-10-08 11:18:24 +0300

    Update brandee.js

--- brandee.js
+++ brandee.js
@@ -30,11 +30,35 @@ client.on('ready', () => {
 
 
 
+const codes = {
+    ' ': '   ',
+    '0': '0⃣',
+    '1': '1⃣',
+    '2': '2⃣',
+    '3': '3⃣',
+    '4': '4⃣',
+    '5': '5⃣',
+    '6': '6⃣',
+    '7': '7⃣',
+    '8': '8⃣',
+    '9': '9⃣',
+    '!': '❕',
+    '?': '❔',
+    '#': '#⃣',
+    '*': '*⃣'
+  };
+  
+  'abcdefghijklmnopqrstuvwxyz'.split('').forEach(c => {
+    codes[c] = codes[c.toUpperCase()] = ` :regional_indicator_${c}:`;
+  });
+  
+  
   client.on('message' , async message => {
+      var prefix = "-";
          if(message.content.startsWith(prefix + "emoji")) {
             let args = message.content.split(" ").slice(1);
     if (args.length < 1) {
-      message.channel.send('ضع الكلمة التي تريد ان يكتبها البوت بالايموجي');
+      message.channel.send('You must provide some text to emojify!');
   }
   
   message.channel.send(
