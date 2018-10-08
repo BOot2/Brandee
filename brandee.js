525e1c8acff880743737a09abed009b0e698ea8e
BOot2 <43100466+BOot2@users.noreply.github.com>
2018-10-08 12:29:12 +0300

    Update brandee.js

--- brandee.js
+++ brandee.js
@@ -133,7 +133,7 @@ if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' لي
          READ_MESSAGES: false
            }).then(() => {
                message.reply("تم اخفاء الشات✅ ");
-	 }
+	 
 }
 });
 
@@ -150,7 +150,7 @@ if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' لي
          READ_MESSAGES: true
            }).then(() => {
                message.reply("تم اظهار الشات✅ ");
-	   }
+	   
 }
 });
 
