cb400939f694d7ab0acae5d1161829e2b14322da
BOot2 <43100466+BOot2@users.noreply.github.com>
2018-10-11 23:59:33 +0300

    Update brandee.js

--- brandee.js
+++ brandee.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const moment = require('moment');
-const args = message.content.split(' ');
 const fs = require('fs'); // npm i fs
 const ms = require('ms'); // npm i ms
 
