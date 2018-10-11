744ecb6bfc4aa869d0758900c821ee8fb7fa77d3
BOot2 <43100466+BOot2@users.noreply.github.com>
2018-10-12 00:08:42 +0300

    Update brandee.js

--- brandee.js
+++ brandee.js
@@ -1333,25 +1333,13 @@ client.on('ready', () => {
 
 
 
-var AsciiTable = require('ascii-data-table').default
-client.on('message', message =>{
 
-    if(message.content == "الرتب"){
-        var 
-        ros=message.guild.roles.size,
-        data = [['Rank', 'RoleName']]
-        for(let i =0;i<ros;i++){
-            if(message.guild.roles.array()[i].id !== message.guild.id){
-         data.push([i,`${message.guild.roles.filter(r => r.position == ros-i).map(r=>r.name)}`])
-        }}
-        let res = AsciiTable.table(data)
 
-        message.channel.send(`**\`\`\`xl\n${res}\`\`\`**`);
-    }
+client.on('message', msg => {
+  if (msg.content === 'نيترو') {
+    msg.reply('تواصل مع <@385529907226345482> ')
+  }
 });
-
-
-
 
 
 
