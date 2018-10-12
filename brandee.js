15da4d4c0a8c884298a84b067df483e5826f80c3
BOot2 <43100466+BOot2@users.noreply.github.com>
2018-10-12 19:52:27 +0300

    Update brandee.js

--- brandee.js
+++ brandee.js
@@ -1375,16 +1375,17 @@ client.on('message', message => {//restart
 
 
 
-client.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
-  if(!message.member.hasPermission("ADMINISTRATOR")){
-  if(message.content.includes("http://") || message.content.includes("https://") || message.content.includes("www") || message.content.includes(".com") || message.content.includes(".gg")){
-  message.delete();
-  message.channel.send("**Your message was deleted because it cointains a link.**").then(msg => msg.delete(5000));
-  }
+module.exports.run = async (bot,message,args) => {
+
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do this.");
+  let botmsg = args.join(" ");
+  message.channel.send(botmsg);
+  message.delete(1);
 }
 
+module.exports.help = {
+  name: "say"
+}
 
 
 
