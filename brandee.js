77700832f30ca1589778b63602071d0c4715d16d
BOot2 <43100466+BOot2@users.noreply.github.com>
2018-10-12 12:47:06 +0300

    Update brandee.js

--- brandee.js
+++ brandee.js
@@ -1333,53 +1333,60 @@ client.on('ready', () => {
 
 
 
+editedcodes.on("message", (edited) => {
+    
+  if (edited.content === '-new') {
+        const reason = edited.content.split(" ").slice(1).join(" ");
+        if (!edited.guild.roles.exists("name", "Say")) return edited.channel.send(`**يجب عمل رتبة بأسم \`فريق الدعم\`**`);
+        if (edited.guild.channels.exists("name", "ticket-" + edited.author.id)) return edited.channel.send(`لديك تذكرة من الأسآس :joy:`);
+        edited.guild.createChannel(`just-ticket`, "text").then(c => {
+            let edited1 = edited.guild.roles.find("name", "Say");
+            let edited2 = edited.guild.roles.find("name", "@everyone");
+            c.overwritePermissions(edited1, {
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });
+            c.overwritePermissions(edited2, {
+                SEND_MESSAGES: false,
+                READ_MESSAGES: false
+            });
+            c.overwritePermissions(message.author, {
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });
+            edited.channel.send(`:white_check_mark: تـم فتح التذكرة , #${c.name}.`);
+            const embed = new Discord.RichEmbed()
+                .setColor(0xCF40FA)
+                .addField(` ${message.author.username}!`, `**مرحبآ    , اكتب  ماتريد وسيتم الرد عليك  . **`)
+                .setTimestamp();
+            c.send({
+                embed: embed
+            });
+        }).catch(console.error); 
+    }
+    if (edited.content === '-close') {
+        if (!edited.channel.name.startsWith(`ticket-`)) return edited.channel.send(`**لا تستطيع :x:**`);
 
+        edited.channel.send(`**هل انت متأكد ؟ ** `)
+            .then((m) => {
+                edited.channel.awaitMessages(response => response.content === 'نعم', {
+                        max: 1,
+                        time: 10000,
+                        errors: ['time'],
+                    })
+                    .then((collected) => {
+                        edited.channel.delete();
+                    })
+                    .catch(() => {
+                        m.edit('وقت الأغلاق أنتهي , لن تمسح التذكرة .').then(m2 => {
+                            m2.delete();
+                        }, 3000);
+                    });
+            });
+        }
 
-client.on('message', OmarMessage => {
-    var OmarNull = null;
-    if(!OmarMessage.author.bot || OmarMessage.content.startsWith(prefix)) {
-        if(OmarMessage.content.split(' ')[0] == `${prefix}Colse-Channels`) {
-            if(OmarMessage.guild.member(OmarMessage.author).hasPermission('MANAGE_CHANNELS') && OmarMessage.guild.member(client.user).hasPermission('MANAGE_CHANNELS')) {
-                let OmarEveryOne = OmarMessage.guild.roles.filter(r => r.name == `@everyone`).first();
-                let i = 0;
-                for (i; i <= OmarMessage.guild.channels.size; i++) {
-                    let channel = OmarMessage.guild.channels.array()[i];
-                    if(i == OmarMessage.guild.channels.size) {
-                        OmarMessage.channel.send(`${OmarMessage.author}, تم اغلاق جميع الرومات`);
-                    } else {
-                        if(channel.type == 'text') {
-                            channel.overwritePermissions(OmarEveryOne, {SEND_MESSAGES:false});
-                        } else if(channel.type == 'voice') {
-                            channel.overwritePermissions(OmarEveryOne, {CONNECT:false});
-                        } else if(channel.type == 'category') {
-                            channel.overwritePermissions(OmarEveryOne, {SEND_MESSAGES:false,CONNECT:false});
-                        } else OmarNull;
-                    };
-                };
-            };
-        };
-        if(OmarMessage.content.split(' ')[0] == `${prefix}Open-Channels`) {
-            if(OmarMessage.guild.member(OmarMessage.author).hasPermission('MANAGE_CHANNELS') && OmarMessage.guild.member(client.user).hasPermission('MANAGE_CHANNELS')) {
-                let OmarEveryOne = OmarMessage.guild.roles.filter(r => r.name == `@everyone`).first();
-                let i = 0;
-                for (i; i <= OmarMessage.guild.channels.size; i++) {
-                    let channel = OmarMessage.guild.channels.array()[i];
-                    if(i == OmarMessage.guild.channels.size) {
-                        OmarMessage.channel.send(`${OmarMessage.author}, تم فتح جميع الرومات`);
-                    } else {
-                        if(channel.type == 'text') {
-                            channel.overwritePermissions(OmarEveryOne, {SEND_MESSAGES:true});
-                        } else if(channel.type == 'voice') {
-                            channel.overwritePermissions(OmarEveryOne, {CONNECT:true});
-                        } else if(channel.type == 'category') {
-                            channel.overwritePermissions(OmarEveryOne, {SEND_MESSAGES:true,CONNECT:true});
-                        } else OmarNull;
-                    };
-                };
-            };
-        };
-    };
-});
+    });
+
 
 
 
