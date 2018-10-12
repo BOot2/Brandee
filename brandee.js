ace9020df620955a4733126fd4875a98ecf87d5e
BOot2 <43100466+BOot2@users.noreply.github.com>
2018-10-12 12:34:18 +0300

    Update brandee.js

--- brandee.js
+++ brandee.js
@@ -1335,10 +1335,30 @@ client.on('ready', () => {
 
 
 
-client.on('message', msg => {
-  if (msg.content === 'نيترو') {
-    msg.reply('تواصل مع <@385529907226345482> ')
-  }
+const arraySort = require('array-sort'),
+table = require('table');
+
+client.on('message' , async (message) => {
+
+    if(message.content.startsWith(prefix + "!??")) {
+
+  let invites = await message.guild.fetchInvites();
+
+    invites = invites.array();
+
+    arraySort(invites, 'uses', { reverse: true });
+
+    let possibleInvites = [['الدعوات', 'الاشخاص']];
+    invites.forEach(i => {
+      possibleInvites.push([i.inviter.username , i.uses]);
+    })
+    const embed = new Discord.RichEmbed()
+    .setColor(0x7289da)
+    .setTitle("دعوات السيرفر")
+    .addField(' المتصدرين' , `${table.table(possibleInvites)}`)
+
+    message.channel.send(embed)
+    }
 });
 
 
