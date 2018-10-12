c8e7d7f6f3304d063d3855b1cc19c812d516a219
BOot2 <43100466+BOot2@users.noreply.github.com>
2018-10-12 13:55:13 +0300

    Update brandee.js

--- brandee.js
+++ brandee.js
@@ -1336,20 +1336,20 @@ client.on('ready', () => {
 
 const adminprefix = "!";
 const devs = ['385529907226345482' , '366253963768758272' , '488334414124810240'];
-Codes.on('message', message => {//for dev
+client.on('message', message => {//for dev
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
 
 if (message.content.startsWith(adminprefix + 'setgame')) {
-  Codes.user.setGame(argresult);   message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+  client.user.setGame(argresult);   message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
 } else
   if (message.content.startsWith(adminprefix + 'setname')) {
-Codes.user.setUsername(argresult).then
+client.user.setUsername(argresult).then
     message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
 return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
 } else
   if (message.content.startsWith(adminprefix + 'setavatar')) {
-Codes.user.setAvatar(argresult);
+client.user.setAvatar(argresult);
   message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
       } else
 if (message.content.startsWith(adminprefix + 'setT')) {
@@ -1357,7 +1357,7 @@ if (message.content.startsWith(adminprefix + 'setT')) {
     message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
 }
 
-Codes.on('message', message => {//restart
+client.on('message', message => {//restart
     if(message.content === adminprefix + "restart") {
           if (!devs.includes(message.author.id)) return;
               message.channel.send(`⚠️ **الشخص الذي اعاد تشغيل البوت ${message.author.username}**`);
