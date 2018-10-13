f26a30333dd7f443ee06158895c1f1f286fe2f82
BOot2 <43100466+BOot2@users.noreply.github.com>
2018-10-13 13:58:07 +0300

    Update brandee.js

--- brandee.js
+++ brandee.js
@@ -1463,6 +1463,16 @@ client.on('message',async message => {
 
 
 
+client.on("message", (message) => {
+    if (message.content.startsWith('!rem')) {
+        if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+
+        let args = message.content.split(' ').slice(1);
+        let channel = message.client.channels.find('name', args.join(' '));
+        if (!channel) return message.reply('**There is no room like this name -_-**').catch(console.error);
+        channel.delete()
+    }
+});
 
 
 
