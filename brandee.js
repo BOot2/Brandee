e033d3e92e8fb123beaf81dbafa9b2562f891f18
BOot2 <43100466+BOot2@users.noreply.github.com>
2018-10-14 00:05:53 +0300

    Update brandee.js

--- brandee.js
+++ brandee.js
@@ -1502,28 +1502,83 @@ msg.delete();
 
 
 
-client.on('message', message => { 
-                            if (message.content.startsWith("!??")) {
-                            message.channel.send({
-                                embed: new Discord.RichEmbed()
-                                    .setAuthor(Majd.user.username,Rocket.user.avatarURL)
-                                    .setThumbnail(Majd.user.avatarURL)
-                                    .setColor('RANDOM')
-                                    .setTitle('Name... Bot ')
-                                    .addField('UPtime', [timeCon(process.uptime())], true)
-                                    .addField('PinG' , [${Date.now() - message.createdTimestamp} + 'MS'], true)
-                                    .addField('RAM"s Usage', [${(process.memoryUsage().rss / 1048576).toFixed()}MB], true)
-                                    .addField('servers', [Majd.guilds.size], true)
-                                    .addField('channels' , [ ${Majd.channels.size} ] , true)
-                                    .addField('Users' ,[ ${Majd.users.size} ] , true)
-                                    .addField('Name' , [ ${Majd.user.tag} ] , true)
-                                    .addField('ID!' , [ ${Majd.user.id} ] , true)
-                                          .addField('Prefix' , [ ${prefix} ] , true)
-                                          .addField('Language' , [ JS ] , true)
-   
+client.on('message', message => {
 
-  }
-  });
+              if(!message.channel.guild) return;
+
+    var prefix = "!";
+
+    if(message.content.startsWith('!!?')) {
+
+    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+
+  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+
+    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+
+
+    let request = `Requested By ${message.author.username}`;
+
+    if (!args) return message.reply('**اكتب شي لي ارسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
+
+    msg.react('✅')
+
+    .then(() => msg.react('❌'))
+
+    .then(() =>msg.react('✅'))
+
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+
+       let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+
+    reaction1.on("collect", r => {
+
+    message.channel.send(`☑ | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.size} Members`).then(m => m.delete(5000));
+
+    message.guild.members.forEach(m => {
+
+    var bc = new
+
+       Discord.RichEmbed()
+
+       .setColor('RANDOM')
+
+         .setTitle('لي دخول السيرفر اضغط هنا')
+       
+       .setURL('https://discord.gg/4EPgdC')
+
+       .addField('Server', message.guild.name)
+
+       .addField('Sender', message.author.username)
+
+       .addField('Message', args)
+
+    m.send({ embed: bc })
+
+    msg.delete();
+
+    })
+
+    })
+
+    reaction2.on("collect", r => {
+
+    message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
+
+    msg.delete();
+
+    })
+
+    })
+
+    }
+
+    });  
+  
 
 
 
