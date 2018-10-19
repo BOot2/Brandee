7636dc9a5f084eda3c10fce99a9132cf730f7e07
BOot2 <43100466+BOot2@users.noreply.github.com>
2018-10-19 18:26:44 +0300

    Update brandee.js

--- brandee.js
+++ brandee.js
@@ -538,7 +538,7 @@ if(ra3d.content.startsWith(prefix + '!cc')) {
     if(!args) return ra3d.channel.send('`يرجي اختيار كم لون `');
              if (!ra3d.member.hasPermission('MANAGE_ROLES')) return ra3d.channel.sendMessage('`**⚠ | `[MANAGE_ROLES]` لا يوجد لديك صلاحية**'); 
               ra3d.channel.send(`**✅ |Created __${args}__ Colors**`);
-                  setInterval(find(){})
+                  setInterval(function(){})
                     let count = 0;
                     let ecount = 0;
           for(let x = 1; x < `${parseInt(args)+1}`; x++){
