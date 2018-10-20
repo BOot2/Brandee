272e9ed0860957532a3f0c2b1b8fa714a6d1d862
TEtr <43100466+BOot2@users.noreply.github.com>
2018-10-20 22:02:43 +0300

    Update brandee.js

--- brandee.js
+++ brandee.js
@@ -1621,49 +1621,70 @@ client.on('message', msg => {
  
 
 
-  client.on('message', message => {
-                    var prefix = "#";
-
-           if (message.content.startsWith(prefix + "id")) {
-                     if(!message.channel.guild) return message.reply(`هذا الأمر فقط ل السيرفرات :x:`);
-
-                message.guild.fetchInvites().then(invs => {
-      let member = client.guilds.get(message.guild.id).members.get(message.author.id);
-      let personalInvites = invs.filter(i => i.inviter.id === message.author.id);
-      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
-      var moment = require('moment');
-      var args = message.content.split(" ").slice(1);
-let user = message.mentions.users.first();
-var men = message.mentions.users.first();
- var heg;
- if(men) {
-     heg = men
- } else {
-     heg = message.author
- }
-var mentionned = message.mentions.members.first();
-  var h;
- if(mentionned) {
-     h = mentionned
- } else {
-     h = message.member
- }
-        moment.locale('ar-TN');
-      var id = new  Discord.RichEmbed()
-       
-    .setColor("#0a0909")
-    .setAuthor(message.author.username, message.author.avatarURL) 
-.addField(': دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true) 
-.addField(': انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true)
-.addField(': عدد الدعوات', inviteCount,false)
-.setFooter("TINEOUT_408")  
-    message.channel.sendEmbed(id);
-})
-}
-    
-
-         
-     });
+     client.on('message', message => {
+ 
+           if (message.content === prefix + "dt") {
+           if (!message.channel.guild) return message.reply('** This command only for servers **');  
+           var currentTime = new Date(),
+              hours = currentTime.getHours() + 4 ,
+              hours2 = currentTime.getHours() + 3 ,
+              hours3 = currentTime.getHours() + 2 ,
+              hours4 = currentTime.getHours() + 3 ,
+              minutes = currentTime.getMinutes(),
+              seconds = currentTime.getSeconds(),
+              Year = currentTime.getFullYear(),
+              Month = currentTime.getMonth() + 1,
+              Day = currentTime.getDate();
+               var h = hours
+    if(hours > 12) {
+                 hours -= 12;
+              } else if(hours == 0) {
+                  hours = "12";
+              }  
+               if(hours2 > 12) {
+                 hours2 -= 12;
+              } else if(hours2 == 0) {
+                  hours2 = "12";
+               
+              }  
+  if(hours3 > 12) {
+                 hours3 -= 12;
+              } else if(hours3 == 0) {
+                  hours3 = "12";
+              }
+              if (minutes < 10) {
+                  minutes = '0' + minutes;
+              }
+   
+   
+              var suffix = 'صباحاَ';
+              if (hours >= 12) {
+                  suffix = 'مساء';
+                  hours = hours - 12;
+              }
+              if (hours == 0) {
+                  hours = 12;
+              }
+   
+   
+                  var Date15= new Discord.RichEmbed()
+                  .setThumbnail("https://i.imgur.com/ib3n4Hq.png")
+                  .setTitle( "「التاريخ  والوقت」")
+                  .setColor('RANDOM')
+                  .setFooter(message.author.username, message.author.avatarURL)
+                  .addField('الامارات',
+                  "「"+ hours + ":" + minutes +":"+ seconds + "」")
+                   .addField('مكه المكرمه',
+                  "「"+ hours2 + ":" + minutes +":"+ seconds  + "」")
+                  .addField('مصر',
+                  "「"+ hours3 + ":" + minutes +":"+ seconds  + "」")
+                   
+                  .addField('Date',
+                  "「"+ Day + "-" + Month + "-" + Year +  "」")
+   
+                   message.channel.sendEmbed(Date15);
+          }
+      });
 
 
 
